a0218316d5611b06628c53942fa76d325246358d
Matt Isaacs <misaacs@gilt.com>
2014-12-17 17:39:52 -0500

    Initial Commit

---
+++ Chaingang/Chaingang.h
@@ -0,0 +1,19 @@
+//
+//  Chaingang.h
+//  Chaingang
+//
+//  Created by Matt Isaacs on 12/17/14.
+//  Copyright (c) 2014 Matt Isaacs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chaingang.
+FOUNDATION_EXPORT double ChaingangVersionNumber;
+
+//! Project version string for Chaingang.
+FOUNDATION_EXPORT const unsigned char ChaingangVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chaingang/PublicHeader.h>
+
+
